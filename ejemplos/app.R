e3b8974244fe943e04c96b5e861689a8fc372312
lucio-cornejo <luciocornejo11@gmailcom>
2022-08-08 19:00:11 -0500

    añadir ejemplo respecto a consulta de sesión 2

--- ejemplos/app.R
+++ ejemplos/app.R
@@ -1,30 +1,5 @@
 library(shiny)
 
-ui <- fluidPage(
-  sliderInput("n", "Number of points", 1, 100, 50),
-  downloadButton("report", "Generate report")
-)
 
-server <- function(input, output, session) {
-  output$report <- downloadHandler(
-    filename = "report.html",
-    content = function(file) {
-      params <- list(n = input$n)
-      
-      id <- showNotification(
-        "Rendering report...", 
-        duration = NULL, 
-        closeButton = FALSE
-      )
-      on.exit(removeNotification(id), add = TRUE)
-
-      rmarkdown::render("report.Rmd", 
-        output_file = file,
-        params = params,
-        envir = new.env(parent = globalenv())
-      )
-    }
-  )
-}
 
 shinyApp(ui, server)
